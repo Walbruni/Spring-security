911f7a22b1290870de3a0e59ab599fea0399e92f
walbrunilucas1 <lucaswalbruni@gmail.com>
2022-08-04 20:43:48 -0300

    autenticação de usuários com banco de dados

---
+++ src/main/java/teste/spring/security/TesteSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package teste.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/main/java/teste/spring/security/init/StartApplication.java
@@ -0,0 +1,37 @@
+package teste.spring.security.init;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import teste.spring.security.model.User;
+import teste.spring.security.repository.UserRepository;
+
+import javax.transaction.Transactional;
+
+@Component
+public class StartApplication implements CommandLineRunner {
+    @Autowired
+    private UserRepository repository;
+    @Transactional
+    @Override
+    public void run(String... args) throws Exception {
+        User user = repository.findByUsername("admin");
+        if (user == null) {
+            user = new User();
+            user.setName("ADMIN");
+            user.setUsername("admin");
+            user.setPassword("master123");
+            user.getRoles().add("MANAGERS");
+            repository.save(user);
+        }
+        user = repository.findByUsername("user");
+        if (user == null) {
+            user = new User();
+            user.setName("USER");
+            user.setUsername("user");
+            user.setPassword("user123");
+            repository.save(user);
+        }
+    }
+}
